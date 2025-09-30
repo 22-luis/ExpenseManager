d74fc81e42118abb0d609aaf900ca2964309e75f
22-luis <00016720@uca.edu.sv>
2025-09-30 07:12:22 -0600

    Add: category and spent dto

---
+++ src/main/java/org/example/expensemanager/entities/dto/Spent/SpentDto.java
@@ -0,0 +1,25 @@
+package org.example.expensemanager.entities.dto.Spent;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+public class SpentDto {
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Spent {
+
+        private Float mount;
+
+        private Date date;
+
+        private String description;
+
+        private String archive;
+
+    }
+}
---
+++ src/main/java/org/example/expensemanager/entities/dto/category/CategoryDto.java
@@ -0,0 +1,19 @@
+package org.example.expensemanager.entities.dto.category;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDto {
+
+    private String name;
+
+    private String description;
+
+    private UUID userId;
+}
