e2b4c5a2550a7919479aef849b5998c047e321b0
22-luis <0006720@uca.edu.sv>
2025-10-04 23:37:23 -0600

    Add: user repository

---
+++ src/main/java/org/example/expensemanager/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package org.example.expensemanager.repositories;
+
+import org.example.expensemanager.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
