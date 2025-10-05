38fa6d97e9eee505403b58f008e2a20781c64bb4
22-luis <0006720@uca.edu.sv>
2025-10-04 23:36:02 -0600

    Update: userDto now have the id

--- src/main/java/org/example/expensemanager/entities/dto/user/UserDto.java
+++ src/main/java/org/example/expensemanager/entities/dto/user/UserDto.java
@@ -5,10 +5,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.expensemanager.entities.Role;
 
+import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
+    private UUID id;
     private String username;
     private String email;
     private String password;
