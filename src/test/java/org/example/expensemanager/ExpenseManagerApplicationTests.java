91dc01a686d1d85edad0998824fa69328926ab35
22-luis <0006720@uca.edu.sv>
2025-10-07 08:20:02 -0600

    Update: CI configurations

--- src/test/java/org/example/expensemanager/ExpenseManagerApplicationTests.java
+++ src/test/java/org/example/expensemanager/ExpenseManagerApplicationTests.java
@@ -2,9 +2,29 @@ package org.example.expensemanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class ExpenseManagerApplicationTests {
+
+    // 1. Define el contenedor de PostgreSQL
+    @Container
+    static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:15-alpine");
+
+    // 2. Sobrescribe la configuración de la BDD para que apunte al contenedor
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+    }
 
     @Test
     void contextLoads() {
