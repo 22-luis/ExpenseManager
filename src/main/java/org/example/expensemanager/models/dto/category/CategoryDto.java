72c22cd49b230ba806f8051d843779ad36f83e44
22-luis <0006720@uca.edu.sv>
2025-10-21 16:21:47 -0600

    Update: the category dto now send the response with the category id

--- src/main/java/org/example/expensemanager/models/dto/category/CategoryDto.java
+++ src/main/java/org/example/expensemanager/models/dto/category/CategoryDto.java
@@ -11,9 +11,12 @@ import java.util.UUID;
 @NoArgsConstructor
 public class CategoryDto {
 
+    private UUID id;
+
     private String name;
 
     private String description;
 
     private UUID userId;
+
 }
