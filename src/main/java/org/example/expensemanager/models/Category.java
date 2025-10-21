ab76727f9052252c54bddb269592a3f6d558b7c9
22-luis <0006720@uca.edu.sv>
2025-10-21 16:19:26 -0600

    Update: set the fields that can be nullables

--- src/main/java/org/example/expensemanager/models/Category.java
+++ src/main/java/org/example/expensemanager/models/Category.java
@@ -16,7 +16,7 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String name;
 
     @Column(name = "description")
